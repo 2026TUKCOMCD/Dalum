f3988f9c16d82c2c3d4d32e220b26832186115fc
djeu1116 <rladbgus781@gmail.com>
2026-02-13 22:01:02 +0900

    fix: ci test

--- Dalum-BE/src/main/java/dalum/dalum/DalumApplication.java
+++ Dalum-BE/src/main/java/dalum/dalum/DalumApplication.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 public class DalumApplication {
 	public static void main(String[] args) {
-
 		SpringApplication.run(DalumApplication.class, args);
 	}
 
