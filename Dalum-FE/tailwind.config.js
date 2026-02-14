ebbd1a3d7829f378326b547a8b09a65d68bfc671
Hyochan <hyochan02@gmail.com>
2026-02-15 03:29:42 +0900

    chore: 디자인시스템 - 타이포그래피, 색상 템플릿 적용

--- Dalum-FE/tailwind.config.js
+++ Dalum-FE/tailwind.config.js
@@ -1,5 +1,4 @@
 /** @type {import('tailwindcss').Config} */
-// 수정 예정
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   important: true,
@@ -7,34 +6,32 @@ export default {
     extend: {
       colors: {
         primary: {
-          50: "#9FD416",
-          40: "#A9E119",
-          30: "#D1F27D",
-          20: "#E3FBA6",
-          10: "#F1FFCE",
+          900: "#1E3A8A",
+        },
+        secondary: {
+          900: "#FAE100",
         },
         gray: {
-          900: "#1A1E22",
-          800: "#33373B",
-          700: "#4C5054",
-          600: "#65696D",
-          500: "#7E8286",
-          400: "#979B9F",
-          300: "#B0B4B8",
-          200: "#C9CDD1",
-          100: "#E9ECEF",
-          50: "#F2F4F6",
+          900: "#222222",
+          800: "#444444",
+          700: "#666666",
+          600: "#888888",
+          500: "#CCCCCC",
+          100: "#E5E5E5",
+          50: "#F1F2F4",
+          0: "#FFFFFF",
         },
-        screen: {
-          0: "#FCFDFF",
-        },
-        alert: {
-          50: "#E84A4A",
+        button: {
+          like: "#EA4335",
+          kakao: "#FAE100",
+          naver: "#03C75A",
+          google: "#F2F2F2",
         },
       },
       fontSize: {
-        h1_24: ["24px", { lineHeight: "140%" }],
-        h2_20: ["20px", { lineHeight: "140%" }],
+        h1_32: ["32px", { lineHeight: "140%" }],
+        h2_24: ["24px", { lineHeight: "140%" }],
+        body_20: ["20px", { lineHeight: "140%" }],
         body_18: ["18px", { lineHeight: "140%" }],
         body_16: ["16px", { lineHeight: "140%" }],
         body_14: ["14px", { lineHeight: "140%" }],
@@ -47,7 +44,13 @@ export default {
         ...theme("colors"),
       }),
       fontFamily: {
-        suite: ["SUITE Variable", "SUITE", "system-ui", "sans-serif"],
+        pretendard: [
+          "Pretendard",
+          "-apple-system",
+          "BlinkMacSystemFont",
+          "system-ui",
+          "sans-serif",
+        ],
       },
       borderColor: (theme) => ({
         ...theme("colors"),
