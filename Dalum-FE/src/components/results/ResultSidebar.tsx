5ababe7eec0b4740a3dbf4f03c93f7c60527374c
Hyochan <hyochan02@gmail.com>
2026-02-15 21:17:14 +0900

    design: 듀프 제품 검색 결과 페이지 사이드바 컴포넌트 퍼블리싱

---
+++ Dalum-FE/src/components/results/ResultSidebar.tsx
@@ -0,0 +1,39 @@
+import HomeIcon from "../../assets/icons/HomeIcon";
+import ImageIcon from "../../assets/icons/ImageIcon";
+import { Button } from "../commons/Button";
+
+const ResultSidebar = () => {
+  return (
+    <div className="h-full w-fit flex flex-col items-start justify-start px-7.5 py-12.5 border-r border-gray-600">
+      <div className="flex flex-col gap-4">
+        {/* 제목 */}
+        <span className="typo-h2_bold24 text-gray-900">| 업로드 이미지</span>
+        {/* 업로드 이미지 */}
+        <div className="w-60 h-60 rounded-sm bg-secondary-900" />
+
+        {/* 버튼 영역 */}
+        <div className="flex flex-col gap-2.5">
+          {/* 버튼 */}
+          <Button
+            variant="primary"
+            size="lg"
+            fullWidth
+            leftIcon={<ImageIcon className="size-4" />}
+          >
+            다른 이미지로 검색
+          </Button>
+          <Button
+            variant="primary"
+            size="lg"
+            fullWidth
+            leftIcon={<HomeIcon className="size-4" />}
+          >
+            마이 페이지로 이동
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ResultSidebar;
