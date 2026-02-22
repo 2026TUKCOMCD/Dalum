1317a941f5a151d73407aa7b85186a31b4c9ae7c
djeu1116 <rladbgus781@gmail.com>
2026-02-22 20:28:18 +0900

    fix: 검색 기록 상세조회시 널값 오류 해결

--- Dalum-BE/src/main/java/dalum/dalum/domain/search_log/dto/response/SearchConditionDto.java
+++ Dalum-BE/src/main/java/dalum/dalum/domain/search_log/dto/response/SearchConditionDto.java
@@ -6,9 +6,9 @@ import lombok.Builder;
 @Builder
 public record SearchConditionDto(
         @Schema(description = "최소가격", example = "10000")
-        int minPrice,
+        Integer minPrice,
 
         @Schema(description = "최대가격", example = "50000")
-        int maxPrice
+        Integer maxPrice
 ) {
 }
