c02a6cafc1ff485761821469cae97280b964277f
Yuhyun <rladbgus781@gmail.com>
2026-05-10 19:13:52 +0900

    refacotr: 네이티브 쿼리 삭제 및 이전코드로 복구

--- Dalum-BE/src/main/java/dalum/dalum/domain/product/repository/ProductRepository.java
+++ Dalum-BE/src/main/java/dalum/dalum/domain/product/repository/ProductRepository.java
@@ -3,6 +3,7 @@ package dalum.dalum.domain.product.repository;
 import dalum.dalum.domain.product.entity.Product;
 import dalum.dalum.domain.product.enums.LargeCategory;
 import dalum.dalum.domain.product.repository.projection.ProductCandidateProjection;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,15 +12,13 @@ import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    @Query(value = "SELECT p.product_id AS id, p.large_category AS largeCategory, p.style AS style, p.material_vector AS materialVector, p.dominant_colors AS dominantColors " +
-            "FROM product p " +
-            "WHERE p.large_category IN :categories AND p.product_id != :excludeId AND p.material_vector IS NOT NULL " +
-            "AND (p.style IS NULL OR p.style IN :compatibleStyles) " +
-            "ORDER BY RANDOM() LIMIT :limit",
-            nativeQuery = true)
-    List<ProductCandidateProjection> findCandidates(@Param("categories") List<String> categories,
+    @Query("SELECT p.id AS id, p.largeCategory AS largeCategory, p.style AS style, p.materialVector AS materialVector, p.dominantColors AS dominantColors " +
+            "FROM Product p " +
+            "WHERE p.largeCategory IN :categories AND p.id != :excludeId AND p.materialVector IS NOT NULL " +
+            "AND (p.style IS NULL OR p.style IN :compatibleStyles)")
+    List<ProductCandidateProjection> findCandidates(@Param("categories") List<LargeCategory> categories,
                                                     @Param("excludeId") Long excludeId,
                                                     @Param("compatibleStyles") List<String> compatibleStyles,
-                                                    @Param("limit") int limit);
+                                                    Pageable pageable);
 
 }
